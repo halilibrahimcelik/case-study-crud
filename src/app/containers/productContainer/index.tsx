bc5dba377d46f6baa5e64d9f97fada863e11df26
halilibrahimcelik <halil.celik139@gmail.com>
2023-11-17 17:00:14 +0300

    feat:added pagination for the products

--- src/app/containers/productContainer/index.tsx
+++ src/app/containers/productContainer/index.tsx
@@ -12,12 +12,16 @@ import SearchForm from "./searchProduct";
 import AddProduct from "./addProduct";
 import ProductCard from "./productCard";
 import Spinner from "@/app/components/spinner";
+import { Products } from "@/lib/types";
+import { Pagination } from "@mui/material";
 type Props = {};
 
 const ProductContainer = (props: Props) => {
   const dispatch = useAppDispatch();
   const loadingStatus = useSelector(getLoading);
-  const productList = useSelector(getProductList);
+  const productList: Products[] = useSelector(getProductList);
+  const [page, setPage] = React.useState(1);
+  const itemsPerPage = 6;
 
   // Memoize the fetchProducsList
   const memoizedFetchProductsList = useMemo(() => {
@@ -28,9 +32,14 @@ const ProductContainer = (props: Props) => {
     memoizedFetchProductsList();
     return () => {};
   }, [memoizedFetchProductsList]);
-
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const displayedProducts = productList.slice(startIndex, endIndex);
   console.log(productList);
-  console.log(loadingStatus);
+  console.log(displayedProducts);
+  const handlePageChange = (event: React.ChangeEvent<any>, newPage: number) => {
+    setPage(newPage);
+  };
   return (
     <Wrapper component="section">
       <div className="py-5 grid grid-cols-1 md:grid-cols-2 gap-10">
@@ -41,9 +50,31 @@ const ProductContainer = (props: Props) => {
         {loadingStatus ? (
           <Spinner />
         ) : (
-          <ul className="grid grid-cols-1 md:grid-cols-2 gap-5">
-            <ProductCard />
-          </ul>
+          <>
+            <ul className="grid grid-cols-1 md:grid-cols-2 gap-5">
+              {productList.length === 0 && loadingStatus ? (
+                <p>Şu an gösterilecek bir ürün bulunmamaktadır</p>
+              ) : (
+                displayedProducts.map((product) => (
+                  <ProductCard key={product.id} product={product} />
+                ))
+              )}
+            </ul>
+            {productList.length > 6 && (
+              <div className="flex py-5 justify-center w-full">
+                <Pagination
+                  variant="text"
+                  size="large"
+                  siblingCount={1}
+                  onChange={handlePageChange}
+                  className="w-fit"
+                  page={page}
+                  count={Math.ceil(productList.length / 6)}
+                  color="secondary"
+                />
+              </div>
+            )}
+          </>
         )}
       </div>
     </Wrapper>
--- src/app/containers/productContainer/productCard/index.tsx
+++ src/app/containers/productContainer/productCard/index.tsx
@@ -1,9 +1,40 @@
+import { Products } from "@/lib/types";
 import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 
-type Props = {};
+type Props = {
+  product: Products;
+};
 
-const ProductCard = (props: Props) => {
-  return <li>ProductCard</li>;
+const ProductCard = ({ product }: Props) => {
+  const { title } = product;
+  return (
+    <li className="flex justify-center">
+      <Card className="w-full sm:max-w-[26rem] md:w-full">
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </li>
+  );
 };
 
 export default ProductCard;
